9267e4be922047d13bac8bbd26f89b1598f015ff
mauricebrinkmann <Mail@mauricebrinkmann.de>
2019-09-11 00:30:11 +0200

    train model with data from ai.json and answer requests

--- api/prediction.py
+++ api/prediction.py
@@ -3,37 +3,21 @@ from sklearn.linear_model import LinearRegression
 
  
 class Prediction:
-  # define data
-  def get_prediction(self):
-    d = {'input 1': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-     'input 2': [0.8, 0.9, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0],
-     'target': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
-
- 
-
-    df = pd.DataFrame(data=d)
-
- 
-
-    # define input and output for the model
-    X = df[['input 1', 'input 2']].values
-    y = df['target'].values
-
- 
-
-    # do the simplest of all machine learning trainings
-    reg = LinearRegression().fit(X, y)
-    reg.score(X, y)
-
- 
-
+  def __init__(self,aijson):  
+        self.aijson = aijson
+        # define data frame
+        self.df = pd.DataFrame(data=self.aijson["data"])
+        # define input and output for the model
+        self.X = self.df[self.aijson["input"]].values
+        self.y = self.df[self.aijson["target"]].values
+        # do the simplest of all machine learning trainings
+        self.reg = LinearRegression().fit(self.X, self.y)
+        print("Model accuracy: %6.2f" % (self.reg.score(self.X, self.y)))
+        
+  def get_prediction(self,input):
     # generate new data - the data that we want to predict
     # (must be in the same format as the input data!!!)
-    new_data = pd.DataFrame({'input 1': [1, 2, 3], 
-                         'input 2': [0.4, 0.5, 0.6]})
-
- 
-
+    new_data = pd.DataFrame(input)
     # make prediction of new data
-    prediction = reg.predict(new_data)
-    return prediction
+    target = reg.predict(new_data)
+    return target
