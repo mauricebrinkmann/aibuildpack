de7d64a44763dcfde6e3ae8bb510d3e64b8a25ad
SebestyenMiklos <sebestyenmiklos1neptun@gmail.com>
2019-09-10 17:10:55 +0200

    Add array handling

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ To start api only:
 1. sudo pip install -U Flask
 2. cd api
 3. env FLASK_APP=server.py flask run
+
+### Flask
+
+https://flask.palletsprojects.com/en/1.1.x/quickstart/
--- api/server.py
+++ api/server.py
@@ -1,7 +1,18 @@
+import json
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/health")
 def hello():
-    return "Hello, World!"
+    return "aibuildpack-api"
+
+@app.route('/', methods = ['POST'])
+def new_data():
+    # CODE
+    req = request.get_data()
+    j = json.loads(req)
+    return {
+        "prediction" : j
+    }
