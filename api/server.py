058ca8ad03d5c4c3a5c366b1ae4af31863702b0a
SebestyenMiklos <sebestyenmiklos1neptun@gmail.com>
2019-09-10 17:31:49 +0200

    Change api startup (#6)
    * Basic pyton server added
    
    * Add array handling
    
    * Change api start up
    
    Start up is compatible with CF now

--- api/server.py
+++ api/server.py
@@ -1,7 +1,29 @@
+import os
+import json
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/health")
 def hello():
-    return "Hello, World!"
+    return "aibuildpack-api"
+
+@app.route('/', methods = ['POST'])
+def new_data():
+    # CODE
+    req = request.get_data()
+    j = json.loads(req)
+    return {
+        "prediction" : j
+    }
+
+if __name__ == "__main__":
+    if os.environ.get('VCAP_SERVICES') is None: # running locally
+        PORT = 8080
+        DEBUG = True
+    else:                                       # running on CF
+        PORT = int(os.getenv("PORT"))
+        DEBUG = False
+
+    app.run(host='0.0.0.0', port=PORT, debug=DEBUG)
